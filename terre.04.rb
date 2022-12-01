95e5decf19c456464ccf7bf3348f738c58146f9c
Thomas SORARU <thomas.soraru@doc-process.com>
2022-12-01 22:24:38 +0100

    Check if number is peer or odd

--- terre.04.rb
+++ terre.04.rb
@@ -1,8 +1,20 @@
-print "Entrer un entier positif"
-i = gets().to_i()
- 
-if i%2 == 0
-  then puts("Votre chiffre est pair")
-else
-  puts("Votre chiffre est impair")
+#Check if number is peer or odd 
+
+class String
+    def is_integer?
+      self.to_i.to_s == self
+    end
+  end
+
+
+for i in ARGV
+    if i.is_integer? == false
+        then puts "Tu ne me la mettra pas a l'envers"
+    elsif i.to_i < 0
+        then puts "Tu ne me la mettra pas a l'envers"
+    elsif i.to_i%2 == 0
+        then puts("Pair")
+    else
+        puts("Impair")
+    end  
 end
