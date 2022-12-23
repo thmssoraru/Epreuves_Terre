1c1117a5b07d9675f192ac3ebbcea49b903ba60b
Thomas SORARU <thomas.soraru@doc-process.com>
2022-12-23 12:07:41 +0100

    Taille d’une chaîne

--- terre.07.rb
+++ terre.07.rb
@@ -1,13 +1,18 @@
-# class String
-#     def is_integer?
-#       self.to_i.to_s == self
-#     end
-#   end
+class String
+    def is_integer?
+      self.to_i.to_s == self
+    end
+end
 
-puts ARGV.length
 
-# if ARGV[0].is_integer? == false
-#     puts ARGV.length
-# else
-#     puts "Pas de chiffres !"
-# end
+if ARGV.length > 1
+        puts "err"
+else
+    for i in ARGV do    
+        if ARGV[0].is_integer? == false
+            puts i.length
+        else
+            puts "Pas de chiffres !"
+        end
+    end
+end
